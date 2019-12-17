dbc271fabab365837e5b9f0982caceee3c34265a
ghazy <ghazy@admins-MacBook-Pro-3.local>
2019-12-17 15:58:30 +0200

    Init playground cpp

---
+++ cmake-build-debug/CMakeFiles/playground_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/playground_cpp.dir/main.cpp.o"
+  "playground_cpp"
+  "playground_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/playground_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
